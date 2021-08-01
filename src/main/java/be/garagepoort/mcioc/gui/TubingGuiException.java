a586c6819f55c42b2057a781dd31a90be37ce7f0
David Maes <david.s.maes@gmail.com>
2021-08-01 14:46:27 +0200

    Use freemarker instead of thymeleaf

--- src/main/java/be/garagepoort/mcioc/gui/TubingGuiException.java
+++ src/main/java/be/garagepoort/mcioc/gui/TubingGuiException.java
@@ -4,4 +4,8 @@ public class TubingGuiException extends RuntimeException{
     public TubingGuiException(String message) {
         super(message);
     }
+
+    public TubingGuiException(String message, Exception e) {
+        super(message, e);
+    }
 }
--- src/main/java/be/garagepoort/mcioc/gui/templates/GuiTemplateResolver.java
+++ src/main/java/be/garagepoort/mcioc/gui/templates/GuiTemplateResolver.java
@@ -1,8 +1,12 @@
 package be.garagepoort.mcioc.gui.templates;
 
 import be.garagepoort.mcioc.IocBean;
+import be.garagepoort.mcioc.TubingPlugin;
 import be.garagepoort.mcioc.gui.TubingGui;
 import be.garagepoort.mcioc.gui.TubingGuiException;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -12,10 +16,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
+import java.io.IOException;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,22 +28,26 @@ import java.util.stream.Collectors;
 @IocBean
 public class GuiTemplateResolver {
 
+    private final Configuration freemarkerConfiguration;
+
+    public GuiTemplateResolver() {
+        freemarkerConfiguration = new Configuration(Configuration.VERSION_2_3_28);
+        freemarkerConfiguration.setClassForTemplateLoading(TubingPlugin.getPlugin().getClass(), "/");
+    }
+
     public TubingGui resolve(String templatePath) {
         return resolve(templatePath, new HashMap<>());
     }
 
     public TubingGui resolve(String templatePath, Map<String, Object> params) {
-        TemplateEngine templateEngine = new TemplateEngine();
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setTemplateMode("HTML");
-        templateEngine.setTemplateResolver(templateResolver);
-
-        Context context = new Context();
-        params.forEach(context::setVariable);
-
-        StringWriter stringWriter = new StringWriter();
-        templateEngine.process(templatePath, context, stringWriter);
-        return parseHtml(stringWriter.toString());
+        try {
+            Template template = freemarkerConfiguration.getTemplate(templatePath);
+            StringWriter stringWriter = new StringWriter();
+            template.process(params, stringWriter);
+            return parseHtml(stringWriter.toString());
+        } catch (IOException | TemplateException e) {
+            throw new TubingGuiException("Could not load template: [" + templatePath + "]", e);
+        }
     }
 
     private TubingGui parseHtml(String html) {
@@ -56,7 +62,7 @@ public class GuiTemplateResolver {
         Element titleElement = tubingGuiElement.selectFirst("title");
         String title = titleElement == null ? "" : titleElement.text();
 
-        TubingGui.Builder builder = new TubingGui.Builder(title, size);
+        TubingGui.Builder builder = new TubingGui.Builder(format(title), size);
         Elements guiItems = tubingGuiElement.select("GuiItem");
         for (Element guiItem : guiItems) {
             int slot = Integer.parseInt(guiItem.attr("slot"));
@@ -67,7 +73,7 @@ public class GuiTemplateResolver {
 
             Element loreElement = guiItem.selectFirst("Lore");
             List<String> loreLines = new ArrayList<>();
-            if(loreElement != null) {
+            if (loreElement != null) {
                 Elements loreLinesElements = loreElement.select("LoreLine");
                 loreLines = loreLinesElements.stream().map(Element::text).collect(Collectors.toList());
             }
