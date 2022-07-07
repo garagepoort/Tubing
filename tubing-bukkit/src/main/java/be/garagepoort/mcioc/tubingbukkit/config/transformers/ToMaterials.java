96217a0eaf9ea52027ee1c5d8f543d5477743620
DavidMaes <david.s.maes@gmail.com>
2022-07-07 13:59:11 +0200

    Add ToMaterials Wildcard transformer

--- tubing-bukkit/src/main/java/be/garagepoort/mcioc/tubingbukkit/config/transformers/ToMaterials.java
+++ tubing-bukkit/src/main/java/be/garagepoort/mcioc/tubingbukkit/config/transformers/ToMaterials.java
@@ -4,23 +4,32 @@ import be.garagepoort.mcioc.configuration.IConfigTransformer;
 import be.garagepoort.mcioc.configuration.files.ConfigurationException;
 import org.bukkit.Material;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Set;
 import java.util.stream.Collectors;
 
-public class ToMaterial implements IConfigTransformer<Object, Object> {
+public class ToMaterials implements IConfigTransformer<Object, Object> {
     @Override
     public Object mapConfig(Object input) {
         if (input == null) {
             return null;
         }
         if (input instanceof String) {
-            return Material.valueOf(((String) input).toUpperCase());
+            return getMaterials((String) input);
         }
         if (input instanceof Collection) {
             return ((Collection<?>) input).stream()
-                .map(s -> Material.valueOf(((String) s).toUpperCase()))
-                .collect(Collectors.toList());
+                .flatMap(s -> getMaterials((String) s).stream())
+                .collect(Collectors.toSet());
         }
         throw new ConfigurationException("ToMaterial transformer needs String or Collection as input, but [" + input.getClass() + "] was given");
+    }
+
+    public Set<Material> getMaterials(String input) {
+        String regex = ("\\Q" + input.toUpperCase() + "\\E").replace("*", "\\E.*\\Q");
+        return Arrays.stream(Material.values())
+            .filter(m -> m.name().matches(regex))
+            .collect(Collectors.toSet());
     }
 }
