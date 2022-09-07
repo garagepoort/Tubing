fc689d8152e79ab166a8ee05d1767452038e436f
DavidMaes <david.s.maes@gmail.com>
2022-09-07 16:55:23 +0200

    Fix subcommand not registering as bean

--- tubing-bukkit/src/main/java/be/garagepoort/mcioc/tubingbukkit/load/TubingBukkitBeanRegistrator.java
+++ tubing-bukkit/src/main/java/be/garagepoort/mcioc/tubingbukkit/load/TubingBukkitBeanRegistrator.java
@@ -4,6 +4,7 @@ import be.garagepoort.mcioc.load.TubingBeanAnnotationRegistrator;
 import be.garagepoort.mcioc.tubingbukkit.annotations.IocBukkitCommandHandler;
 import be.garagepoort.mcioc.tubingbukkit.annotations.IocBukkitListener;
 import be.garagepoort.mcioc.tubingbukkit.annotations.IocBukkitMessageListener;
+import be.garagepoort.mcioc.tubingbukkit.annotations.IocBukkitSubCommand;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,6 +13,6 @@ public class TubingBukkitBeanRegistrator implements TubingBeanAnnotationRegistra
 
     @Override
     public List<Class> getAnnotations() {
-        return Arrays.asList(IocBukkitListener.class, IocBukkitMessageListener.class, IocBukkitCommandHandler.class);
+        return Arrays.asList(IocBukkitListener.class, IocBukkitMessageListener.class, IocBukkitCommandHandler.class, IocBukkitSubCommand.class);
     }
 }
